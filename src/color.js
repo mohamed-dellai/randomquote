c5e354c079ce1496f0e939ab6721c12ae6809b3e
Mohamed Dellai <mohameddellai6@gmail.com>
2022-01-14 20:02:02 +0100

    seconde

---
+++ src/color.js
@@ -0,0 +1,3 @@
+let color1=["DarkSalmon",'MediumPurple','RoyalBlue','LightSeaGreen','Goldenrod'];
+
+export default color1;
---
+++ src/data.js
@@ -0,0 +1,18 @@
+  let data=[{
+    text:'Limitations live only in our minds. But if we use our imaginations, our possibilities become limitless.',
+    auth:'- Jamie Paolinetti'
+  },
+  {
+  text:'It’s your place in the world; it’s your life. Go on and do all you can with it, and make it the life you want to live.',
+    auth:'- Mae Jemison'
+  },
+  {
+    text:'Life is what we make it, always has been, always will be.',
+    auth:'- Grandma Moses'
+  },
+  {
+    text:'I attribute my success to this: I never gave or took any excuse.',
+    auth:'- Florence Nightingale'
+  }
+  ]
+  export default data ;
